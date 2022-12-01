8689ad2a97ebc8f1ac77b830dc41010d6d4a3065
ScotyXD <ScottHarrison2020@outlook.com>
2022-12-01 11:44:48 +0000

    First Assignment
    First Javascript Assignment upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+var Family = "The Harrisons", Dad = "Scott", Mum = "Monique", Daughter1 = "Skylar-Rose", Daughter2 = "Aurora-Rose"; // Assigning string value to variables
+var Daughter1= Daughter1.fontcolor("blue") //Assigning font color blue method to daughter1
+document.write(Daughter1);// creating the funtion write daughter 1 and expression 3+3
+document.write(1.5 + 1.5);
+window.alert("Hello World!");
