b3cbbfa2557552a493f64cf75fc56ec7f9d01581
ScotyXD <ScottHarrison2020@outlook.com>
2023-03-02 13:10:43 +0000

    updated TTT Assignment

--- JavaScript_Project/TickTacToe/js/tictactoe.js
+++ JavaScript_Project/TickTacToe/js/tictactoe.js
@@ -75,22 +75,22 @@ function placeXOrO(squareNumber) {
           //drawWinLine function is called to draw line if condition is met.
           function checkWinConditions() {
   
-                    if       (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100); }
-                    else if  (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 304, 558, 304); }
-                     else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 508, 558, 508); }
-                     else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(100, 50, 100, 558); }
-                     else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine(304, 50, 304, 558); }
-                     else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine(508, 50, 508, 558); }
-                     else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(100, 508, 510, 90); }
-                     else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 100, 520, 520);}
-                     else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(50, 100, 558, 100);}
-                     else if (arrayIncludes('0O', '1O', '2O')) { drawWinLine(50, 304, 558, 304);}
-                     else if (arrayIncludes('3O', '4O', '5O')) { drawWinLine(50, 508, 558, 508);}
-                     else if (arrayIncludes('6O', '7O', '8O')) { drawWinLine(100, 50, 100, 558);}
-                     else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(304, 50, 304, 558);}
-                     else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(508, 50, 508, 558);}
-                     else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine(100, 508, 510, 90);}
-                     else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(100, 100, 520, 520);}
+                     if       (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100); }
+                     else if  (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304); }
+                     else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 508, 558, 508); }
+                     else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine(100, 50, 100, 558); }
+                     else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine(304, 50, 304, 558); }
+                     else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(508, 50, 508, 558); }
+                     else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 508, 510, 90); }
+                     else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(100, 100, 520, 520);}
+                     else if (arrayIncludes('0O', '1O', '2O')) { drawWinLine(50, 100, 558, 100);}
+                     else if (arrayIncludes('3O', '4O', '5O')) { drawWinLine(50, 304, 558, 304);}
+                     else if (arrayIncludes('6O', '7O', '8O')) { drawWinLine(50, 508, 558, 508);}
+                     else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100, 50, 100, 558);}
+                     else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 558);}
+                     else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine(508, 50, 508, 558);}
+                     else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(100, 508, 510, 90);}
+                     else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine(100, 100, 520, 520);}
                      
                      
                     //This condition check for tie. if none of the above conditions register
